dc5ca95e8332de430ade033558991da02ee8070b
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-24 22:25:29 +0530

    Update 6_Search_values_in_Dictionary.py

--- 6_Search_values_in_Dictionary.py
+++ 6_Search_values_in_Dictionary.py
@@ -5,3 +5,9 @@ for i in range(a):
   v = input()
   myDict.update({k:v})
   print(myDict)
+def searchindict(dictionary,values):
+  for key in dicionary:
+    if dictionary[key] == values:
+      return key,value
+    else:
+      return "value does not exist"
