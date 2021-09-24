7012886367c4163d88ba5921ce83a2439a60fa39
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-24 22:29:10 +0530

    Create 5_Delete_Remove_value_From_Dictionary.py

---
+++ 5_Delete_Remove_value_From_Dictionary.py
@@ -0,0 +1,7 @@
+myDict = {}
+a = int(input("Enter the values for dictionary"))
+for i in range(a):
+  k = int(input("Enter key values"))
+  v = int(input("Enter value to input"))
+  myDict.upadte({k:v})
+  print(myDict)
